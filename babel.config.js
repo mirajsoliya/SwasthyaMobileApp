9ea3c0259ecb75caf8192bb8647ba998d707cc19
my1243 <mihiryarra@gmail.com>
2023-01-19 23:53:49 +0530

    Prescription pg updated

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,8 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      'nativewind/babel'
-
+      'nativewind/babel',
+        'macros'
     ],
   };
 
